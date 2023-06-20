d52ed8577f9ca9c3db7fd36181b119db0e7f5e64
Kasun Wickramanayke <kwickramanayake007@gmail.com>
2023-06-21 08:56:06 +1200

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Kasun Wickramanayake on 21/06/23.
+//
+
+import Foundation
+
+struct FeedItm {
+    
+    let id:UUID
+    let description :String?
+    let location:String?
+    let imageURL :URL
+    
+}
