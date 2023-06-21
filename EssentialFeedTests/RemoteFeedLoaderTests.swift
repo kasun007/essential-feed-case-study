ec9b1c2eba631a60a7f5f62dbfef9a5a2e804747
Kasun Wickramanayke <kwickramanayake007@gmail.com>
2023-06-21 21:31:29 +1200

    Refactor Remote Feedback Loader

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -35,7 +35,7 @@ protocol HTTPClient{
 }
 
 
-class HTTPClientSpy :HTTPClient {
+private class HTTPClientSpy :HTTPClient {
     
     var requestedURL :URL?
     
@@ -62,12 +62,20 @@ class RemoteFeedLoaderTests:XCTestCase{
     
     func test_load_requestDataFromURL(){
        
-        let url = URL(string: "https://a-givfffen-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url:url  ,client: client)
+        
+        let url = URL (string:"https://a-given-url.com")!
+        let (sut,client) = makeSUT(url:url)
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // mark Helpers
+    private func makeSUT(url:URL = URL(string: "https://a-url.com")!)->(sut:RemoteFeedLoader,client:HTTPClientSpy){
+        
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url:url,client: client)
+        return(sut,client)
     }
     
 }
